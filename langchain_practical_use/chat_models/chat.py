60c442ca33ae5112247b6729b1b1ca102c2d5537
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-07-20 18:35:34 +0530

    feat: add HuggingFace and MistralAI chat model implementations and update project dependencies

--- langchain_practical_use/chat_models/chat.py
+++ langchain_practical_use/chat_models/chat.py
@@ -3,8 +3,8 @@ load_dotenv(find_dotenv())
 
 from langchain_mistralai import ChatMistralAI
 
-model = ChatMistralAI(model="mistral-small-latest",temperature=0.7) # or "mistral-large-latest"
-
+model = ChatMistralAI(model="mistral-small-latest",temperature=0.7, max_tokens=75) 
+ 
 print(model)
 
 response = model.invoke("write poem on  alps! in stoic tone ")
